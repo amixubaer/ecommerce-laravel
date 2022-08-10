49aa343e7a99edbbf18ba58d8c85361fc723e422
amixubaer <abujubaer90@gmail.com>
2022-08-10 17:07:00 +0600

    booking manage

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-    <title>Bazar </title>
+    <title>Gentelella Alela! | </title>
 
     <!-- Bootstrap -->
     <link href="{{asset('admin_theme/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -33,21 +33,17 @@
   <body class="nav-md">
     <div class="container body">
       <div class="main_container">
-<!-- sidebar -->
-@include('admin.layout.sidebar')
-
- <!-- header -->
- @include('admin.layout.header')
+        @include('admin.layout.sidebar')
+        @include('admin.layout.header')
 
         <!-- page content -->
         <div class="right_col" role="main">
-
-@yield('content')
-
+          @yield('content')
         </div>
+        <!-- /page content -->
+
         <!-- footer content -->
         <footer>
-
           <div class="clearfix"></div>
         </footer>
         <!-- /footer content -->
@@ -58,42 +54,11 @@
     <script src="{{asset('admin_theme/vendors/jquery/dist/jquery.min.js')}}"></script>
     <!-- Bootstrap -->
     <script src="{{asset('admin_theme/vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-    <!-- FastClick -->
-    <script src="{{asset('admin_theme/vendors/fastclick/lib/fastclick.js')}}"></script>
     <!-- NProgress -->
     <script src="{{asset('admin_theme/vendors/nprogress/nprogress.js')}}"></script>
-    <!-- Chart.js -->
-    <script src="{{asset('admin_theme/vendors/Chart.js/dist/Chart.min.js')}}"></script>
-    <!-- gauge.js -->
-    <script src="{{asset('admin_theme/vendors/gauge.js/dist/gauge.min.js')}}"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="{{asset('admin_theme/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
-    <!-- iCheck -->
-    <script src="{{asset('admin_theme/vendors/iCheck/icheck.min.js')}}"></script>
-    <!-- Skycons -->
-    <script src="{{asset('admin_theme/vendors/skycons/skycons.js')}}"></script>
-    <!-- Flot -->
-    <script src="{{asset('admin_theme/vendors/Flot/jquery.flot.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/Flot/jquery.flot.pie.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/Flot/jquery.flot.time.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/Flot/jquery.flot.stack.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/Flot/jquery.flot.resize.js')}}"></script>
-    <!-- Flot plugins -->
-    <script src="{{asset('admin_theme/vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/flot.curvedlines/curvedLines.js')}}"></script>
-    <!-- DateJS -->
-    <script src="{{asset('admin_theme/vendors/DateJS/build/date.js')}}"></script>
-    <!-- JQVMap -->
-    <script src="{{asset('admin_theme/vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="{{asset('admin_theme/vendors/moment/min/moment.min.js')}}"></script>
-    <script src="{{asset('admin_theme/vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
 
     <!-- Custom Theme Scripts -->
     <script src="{{asset('admin_theme/build/js/custom.min.js')}}"></script>
-
+	@stack('footer-script')
   </body>
 </html>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -25,24 +25,29 @@
         <div class="menu_section">
           <h3>General</h3>
           <ul class="nav side-menu">
-            <li><a href="{{route('admin.dashboard')}}"><i class="fa fa-home"></i> Dashboard</span></a>
+            <li><a href="{{route('admin.dashboard')}}"><i class="fa fa-home"></i> Dashboard </a>
             </li>
-            <li class=""><a><i class="fa fa-home"></i>Category Manager <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu" style="display: none;">
-                  <li><a href="{{route('category.list')}}">List</a></li>
-                  <li><a href="{{route('category.create')}}">Create</a></li>
-                </ul>
+            <li class=""><a><i class="fa fa-home"></i>Category<span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu" style="display: none;">
+                <li><a href="{{route('category.list')}}">List</a></li>
+                <li><a href="{{route('category.create')}}">Create</a></li>
+              </ul>
             </li>
-            <li class=""><a><i class="fa fa-home"></i>Product Manager <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu" style="display: none;">
-                  <li><a href="{{route('product.list')}}">List</a></li>
-                  <li><a href="{{route('product.create')}}">Create</a></li>
-                </ul>
-              </li>
-            <li class=""><a><i class="fa fa-home"></i>User Manager <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu" style="display: none;">
-                    <li><a href="{{route('admin.users')}}">List</a></li>
-                </ul>
+            <li class=""><a><i class="fa fa-home"></i>Product<span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu" style="display: none;">
+                <li><a href="{{route('product.list')}}">List</a></li>
+                <li><a href="{{route('product.create')}}">Create</a></li>
+              </ul>
+            </li>
+            <li class=""><a><i class="fa fa-home"></i>User<span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu" style="display: none;">
+                <li><a href="{{route('admin.users')}}">List</a></li>
+              </ul>
+            </li>
+            <li class=""><a><i class="fa fa-home"></i>Product Booking<span class="fa fa-chevron-down"></span></a>
+              <ul class="nav child_menu" style="display: none;">
+                <li><a href="{{route('booking.products')}}">List</a></li>
+              </ul>
             </li>
           </ul>
         </div>
